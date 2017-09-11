76bec15d2443ababb23bb41cc62c2d3ad1ae8193
Patricio López Juri <lopezjuripatricio@gmail.com>
2017-09-11 20:39:00 -0300

    Add test docs

--- HighLife/src/CreatesHighLifeBoardTestCase.java
+++ HighLife/src/CreatesHighLifeBoardTestCase.java
@@ -19,6 +19,9 @@ import junit.framework.Assert;
  *
  */
 public class CreatesHighLifeBoardTestCase {
+	/**
+	 * Creating a 0x0 board should run without creating exceptions. This is a border-case. 
+	 */
 	@Test
 	public void testCreateSmallestsBoard() {
 		int length = 0, width = 0;
@@ -32,6 +35,9 @@ public class CreatesHighLifeBoardTestCase {
 		}
 	}
 
+	/**
+	 * Should create a base 1x1 board.
+	 */
 	@Test
 	public void testCreateUnidimentionalBoard() {
 		int length = 1, width = 1;
@@ -41,16 +47,13 @@ public class CreatesHighLifeBoardTestCase {
 		for (int i = 0; i < data.length; i++) {
 			assertEquals("Row/Col should be unidimentional", data[i].length, 1);
 		}
-		
-		board = new HighLifeBoard(data);
-		assertEquals("Row/Col should be unidimentional", data.length, 1);
-		for (int i = 0; i < data.length; i++) {
-			assertEquals("Row/Col should be unidimentional", data[i].length, 1);
-		}
 	}
 
+	/**
+	 * Should create a normal sized board with no starting life on it.
+	 */
 	@Test
-	public void testCreateBoardRandomConstructor() {
+	public void testCreateBoard() {
 		int length = 10, width = 10;
 		HighLifeBoard board = new HighLifeBoard(length, width, false);
 
@@ -63,6 +66,10 @@ public class CreatesHighLifeBoardTestCase {
 		}
 	}
 
+	/**
+	 * Should create a normal board with a chance of having life.
+	 * This test can fail because is non-deterministic. Please a couple of times.
+	 */
 	@Test
 	public void testCreateBoardRandomConstructorAndTrue() {
 		int length = 10, width = 10;
--- HighLife/src/GameRulesCaseTest.java
+++ HighLife/src/GameRulesCaseTest.java
@@ -7,6 +7,14 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class GameRulesCaseTest {
+	/**
+	 * Create a sample board with some life on it (if required) and a center being (if required).
+	 * @param length Board size
+	 * @param width Board size
+	 * @param n Number of starting life entities.
+	 * @param populate True to start with a starting and centered (test subject) being.
+	 * @return
+	 */
 	static HighLifeBoard createBoard(int length, int width, int n, boolean populate) {
 		boolean[][] data = new boolean[length][width];
 		for (int i = 0; i < length; i++) {
@@ -24,27 +32,35 @@ public class GameRulesCaseTest {
 		return new HighLifeBoard(data);
 	}
 
+	/**
+	 * Test born rule according to HighLife rules.
+	 * "... a cell is born if it has 3 or 6 neighbors." 
+	 */
 	@Test
 	public void testBornRule() {
 		for (int i = 0; i < 9; i++) {
 			HighLifeBoard board = createBoard(3, 3, i, false);
 			assertEquals("Cell should stil dead", board.isAlive(1, 1), false);
 
-			if (3 <= i && i <= 6) {
+			if (3 == i || i == 6) {
 				assertEquals("Cell should born", true, board.shouldBeBorn(1, 1));
 			} else {
-				assertEquals("Cell should stil dead", false, board.shouldBeBorn(1, 1));
+				assertEquals("Cell should stay dead", false, board.shouldBeBorn(1, 1));
 			}
 		}
 	}
 
+	/**
+	 * Test survive rule according to HighLife rules.
+	 * "... survives if it has 2 or 3 neighbors." 
+	 */
 	@Test
 	public void testSurviveRule() {
 		for (int i = 0; i < 9; i++) {
 			HighLifeBoard board = createBoard(3, 3, i, true);
 			assertEquals("Cell should stil alive", true, board.isAlive(1, 1));
 
-			if (2 <= i && i <= 3) {
+			if (2 == i || i == 3) {
 				assertEquals("Cell should survive", true, board.shouldSurvive(1, 1));
 			} else {
 				assertEquals("Cell should die", false, board.shouldSurvive(1, 1));
